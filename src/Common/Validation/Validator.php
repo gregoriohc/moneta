07cf2102a0950b705896d8824158b1cad14c16f1
Gregorio Hernández Caso <gregoriohc@gmail.com>
2018-03-11 20:12:18 +0100

    Add missing Validator message

--- src/Common/Validation/Validator.php
+++ src/Common/Validation/Validator.php
@@ -71,6 +71,7 @@ class Validator
         "required_if"      => "The :attribute field is required when :other is :value.",
         "required_with"    => "The :attribute field is required when :values is present.",
         "required_without" => "The :attribute field is required when :values is not present.",
+        "required_without_all" => "The :attribute field is required when :values are not present.",
         "same"             => "The :attribute and :other must match.",
         "size"             => [
             "numeric" => "The :attribute must be :size.",
